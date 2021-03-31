e6d4e177f86e32f3844b6ff3b4739f5afb82843c
Alexander Borovtsov <Alexander.Borovtsov@dataart.com>
2021-03-31 18:05:19 +0300

    Sql-Metadata
    - update regex, aliases2tables

--- meta_export.py
+++ meta_export.py
@@ -4,9 +4,10 @@ import os
 import pandas as pd
 
 class MetaExporter:
-    def __init__(self, root_script_directory, include_scripts = None):
+    def __init__(self, root_script_directory, include_scripts = None, table_aliases = None):
         self.root_directory = root_script_directory
         self.include_scripts = include_scripts
+        self.table_aliases = table_aliases
 
     def __get_sql_paths(self):
         sql_paths = list(Path(self.root_directory).rglob("*.[sS][qQ][lL]"))
@@ -17,7 +18,7 @@ class MetaExporter:
 
     def to_csv(self, path):
         sql_paths = self.__get_sql_paths()
-        metas = [SQLMetadata(path).as_json() for path in sql_paths]
+        metas = [SQLMetadata(path, self.table_aliases).as_json() for path in sql_paths]
         df = pd.DataFrame(metas)
         df.sort_values(by='name', inplace=True)
         df.reset_index(drop=True, inplace=True)
--- sql_metadata.py
+++ sql_metadata.py
@@ -3,9 +3,9 @@ import json
 import os
 
 class SQLMetadata:
-    def __init__(self, sql_path):
+    def __init__(self, sql_path, table_aliases = None):
         # todo: validate SP exists and not empty
-
+        self.table_aliases = table_aliases
         lines = []
         with open(sql_path, 'r') as file:
             lines = [line for line in file.readlines() if not re.match(r'^\s*(--|/\*)', line)] # remove the commented lines
@@ -14,7 +14,13 @@ class SQLMetadata:
         self.name = os.path.basename(sql_path).replace('.sql', '')
 
     def __get_tables(self):
-        pattern = r"(^from\s+|\s+from\s+|^join\s+|\s+join\s+|delete\s+from\s+|insert\s+into\s+|^update\s+|\s+update\s+|merge\s+into\s+)([^\s\+']+)"
+        pattern = r"(^from\s+|\s+from\s+|" + \
+                  r"^join\s+|\s+join\s+|" + \
+                  r"delete\s+from\s+|" + \
+                  r"insert\s+into\s+|" + \
+                  r"\s+update\s+top\s*\(\d+\)\s+|^update\s+|\s+update\s+|" + \
+                  r"merge\s+into\s+)([^\s\+']+)"
+
         table_names = re.findall(pattern, self.sql, re.IGNORECASE)
         table_names = set([self.__clean_up(table_name[1]).lower() for table_name in table_names])
         table_names = list(filter(lambda i: 
@@ -22,9 +28,18 @@ class SQLMetadata:
             not i.startswith('@') and 
             not i.startswith('#') and 
             not i.startswith('sys.') and 
-            not i.lower() == 'sysobjects' and
+            i.lower() != 'sysobjects' and
             not i.lower().startswith('cte') and
-            not i.lower().endswith('cte'), table_names))
+            not i.lower().endswith('cte') and
+            not i.lower().endswith('cursor') and
+            i.lower() != 'of', table_names))
+
+        if self.table_aliases:
+            for alias in self.table_aliases:
+                table_names = [self.table_aliases[alias] if alias.lower() == name else name for name in table_names]
+            
+            table_names = list(set(table_names))
+
         table_names.sort()
         return table_names
 
