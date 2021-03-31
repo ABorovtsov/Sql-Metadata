0c5924303ca65793d9f73a456dd46a21fd2ef512
Alexander Borovtsov <Alexander.Borovtsov@dataart.com>
2021-03-31 19:12:28 +0300

    Sql-Metadata
    - update regex

--- sql_metadata.py
+++ sql_metadata.py
@@ -2,6 +2,7 @@ import re
 import json
 import os
 
+
 class SQLMetadata:
     def __init__(self, sql_path, table_aliases = None):
         # todo: validate SP exists and not empty
@@ -10,6 +11,8 @@ class SQLMetadata:
         with open(sql_path, 'r') as file:
             lines = [line for line in file.readlines() if not re.match(r'^\s*(--|/\*)', line)] # remove the commented lines
         
+        variables = [line.lower().split()[1:3] for line in lines if re.match(r'\s*declare\s+@', line, re.IGNORECASE)]
+        self.variables = {variable[0]:variable[1] for variable in variables}
         self.sql = ' '.join(lines).replace('\n', ' ').replace('\r', ' ')
         self.name = os.path.basename(sql_path).replace('.sql', '')
 
@@ -19,13 +22,14 @@ class SQLMetadata:
                   r"delete\s+from\s+|" + \
                   r"insert\s+into\s+|" + \
                   r"\s+update\s+top\s*\(\d+\)\s+|^update\s+|\s+update\s+|" + \
-                  r"merge\s+into\s+)([^\s\+']+)"
+                  r"merge\s+into\s+)([\s\+']+)(\S+)"
 
         table_names = re.findall(pattern, self.sql, re.IGNORECASE)
-        table_names = set([self.__clean_up(table_name[1]).lower() for table_name in table_names])
+        table_names = set([self.__clean_up(table_name[2]).lower() for table_name in table_names])
         table_names = list(filter(lambda i: 
             not i.startswith('(') and 
-            not i.startswith('@') and 
+            not (i.startswith('@') and 'table' in self.variables.get(i, '').lower()) and 
+            not (i.startswith('@') and i not in self.variables) and 
             not i.startswith('#') and 
             not i.startswith('sys.') and 
             i.lower() != 'sysobjects' and
